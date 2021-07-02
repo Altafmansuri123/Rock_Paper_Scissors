87ce72b3192739ed205280c9e37d1ab1957acbcb
Altaf <altafmansuri780@gmail.com>
2021-07-02 12:26:11 +0530

    Modified app.js and 5 round game functionality added

--- app.js
+++ app.js
@@ -14,19 +14,24 @@ function computerPlay() {
 
 }
 
+let result = "";
+let winCount = 0;
+let loseCount = 0;
 
 function playRound(playerSelection, computerSelection) {
     // your code here!
-    let result = "";
+
 
     if (playerSelection !== computerSelection) {
         if (playerSelection === "ROCK" && computerSelection === "SCISSOR" ||
             playerSelection === "PAPER" && computerSelection === "ROCK" ||
             playerSelection === "SCISSOR" && computerSelection === "PAPER") {
+            winCount++;
             result = `You Win! ${playerSelection} beats ${computerSelection} `;
             return result;
         }
         else {
+            loseCount++;
             result = `You Lose! ${computerSelection} beats ${playerSelection}`;
             return result;
         }
@@ -40,6 +45,25 @@ function playRound(playerSelection, computerSelection) {
 
 }
 
-const playerSelection = prompt("Enter Your Choice Either Rock,Paper or Scissor");
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection.toUpperCase(), computerSelection.toUpperCase()));
+// const playerSelection = prompt("Enter Your Choice Either Rock,Paper or Scissor");
+// const computerSelection = computerPlay();
+// console.log(playRound(playerSelection.toUpperCase(), computerSelection.toUpperCase()));
+game();
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Enter Your Choice Either Rock,Paper or Scissor");
+        const computerSelection = computerPlay();
+        console.log(playRound(playerSelection.toUpperCase(), computerSelection.toUpperCase()));
+    }
+
+    if (winCount > loseCount) {
+        console.log("HURRAY YOU WIN!!!!");
+    }
+    else if (winCount < loseCount) {
+        console.log("YOU LOSE BETTER LUCK NEXT TIME!");
+    }
+    else {
+        console.log("MATCH TIE");
+    }
+}
