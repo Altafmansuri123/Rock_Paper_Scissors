a0434b08448c3175599b338c7b7bebb490792749
Altaf <altafmansuri780@gmail.com>
2021-07-02 11:44:24 +0530

    modified app.js game functionality works fine for a single round match

--- app.js
+++ app.js
@@ -8,10 +8,38 @@ function computerPlay() {
         return "Paper";
     }
     else {
-        return "Scissors";
+        return "Scissor";
     }
 
 
 }
 
 
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+    let result = "";
+
+    if (playerSelection !== computerSelection) {
+        if (playerSelection === "ROCK" && computerSelection === "SCISSOR" ||
+            playerSelection === "PAPER" && computerSelection === "ROCK" ||
+            playerSelection === "SCISSOR" && computerSelection === "PAPER") {
+            result = `You Win! ${playerSelection} beats ${computerSelection} `;
+            return result;
+        }
+        else {
+            result = `You Lose! ${computerSelection} beats ${playerSelection}`;
+            return result;
+        }
+
+    } else {
+        result = "Match Draws";
+        return result;
+    }
+
+
+
+}
+
+const playerSelection = prompt("Enter Your Choice Either Rock,Paper or Scissor");
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection.toUpperCase(), computerSelection.toUpperCase()));
