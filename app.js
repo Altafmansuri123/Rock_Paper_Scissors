ce049b15f82d8af1229b109b7ab4a63a6d5ded76
Altaf <altafmansuri780@gmail.com>
2021-07-02 10:17:44 +0530

    functional logic for rock paper and Scissor created successfully

---
+++ app.js
@@ -0,0 +1,17 @@
+function computerPlay() {
+    let rand = Math.floor(Math.random() * 3 + 1);
+    console.log(rand);
+    if (rand == 1) {
+        return "Rock";
+    }
+    else if (rand == 2) {
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+
+
+}
+
+
