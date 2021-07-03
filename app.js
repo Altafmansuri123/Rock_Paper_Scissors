d305668ab6784240347003d0bf9d39f371fc1461
Altaf <altafmansuri780@gmail.com>
2021-07-03 19:37:31 +0530

    DOM funtionalities Added

--- app.js
+++ app.js
@@ -28,11 +28,13 @@ function playRound(playerSelection, computerSelection) {
             playerSelection === "PAPER" && computerSelection === "ROCK" ||
             playerSelection === "SCISSOR" && computerSelection === "PAPER") {
             playerScore++;
+            p1Score.innerHTML = `Player Score : ${playerScore}`
             result = `You Win! ${playerSelection} beats ${computerSelection} `;
             return result;
         }
         else {
             computerScore++;
+            c1Score.innerHTML = `Computer Score : ${computerScore}`
             result = `You Lose! ${computerSelection} beats ${playerSelection}`;
             return result;
         }
@@ -50,35 +52,90 @@ function playRound(playerSelection, computerSelection) {
 // const playerSelection = prompt("Enter Your Choice Either Rock,Paper or Scissor");
 // const computerSelection = computerPlay();
 // console.log(playRound(playerSelection.toUpperCase(), computerSelection.toUpperCase()));
-game();
+let p1Score = document.querySelector("#playerScore");
+let c1Score = document.querySelector("#computerScore");
+let singleRoundResult = document.querySelector("#result");
+let overallResult = document.querySelector("#overallResult");
 
-function game() {
-    for (i = 1; i < 10; i++) {
+const buttons = document.querySelectorAll("button");
+for (let button of buttons) {
+    button.addEventListener("click", function () {
 
-        const playerSelection = prompt("Enter Your Choice Either Rock,Paper or Scissor");
+        game1(button.innerHTML);
+    });
+}
 
-        if (playerSelection.toUpperCase() !== "ROCK" &&
-            playerSelection.toUpperCase() !== "PAPER" &&
-            playerSelection.toUpperCase() !== "SCISSOR") {
-            alert("invalid input! Please Enter a valid input");
-            i--;
-            continue;
+function game1(playerSelection) {
+    const computerSelection = computerPlay();
+    if (playerScore === 5 || computerScore === 5) {
+        disableButtons();
+        if (playerScore === 5) {
+
+            overallResult.innerHTML = "You won the game! Reload the page to play again";
         }
-
-
-        const computerSelection = computerPlay();
-        console.log(playRound(playerSelection.toUpperCase(), computerSelection.toUpperCase()));
-
-        if (playerScore === 5 || computerScore === 5) {
-            break;
+        else {
+            overallResult.innerHTML = "You Lose the game! Reload the page to play again";
         }
+        return;
     }
-
-    if (playerScore === 5) {
-        console.log("You won the game! Reload the page to play again");
-    }
-    else {
-        console.log("You Lose the game! Reload the page to play again");
-    }
+    singleRoundResult.innerHTML = playRound(playerSelection.toUpperCase(), computerSelection.toUpperCase());
 
 }
+
+
+function disableButtons() {
+    for (let button of buttons) {
+        button.disabled = true;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function game() {
+//     for (i = 1; i < 10; i++) {
+
+//         const playerSelection = prompt("Enter Your Choice Either Rock,Paper or Scissor");
+
+//         if (playerSelection.toUpperCase() !== "ROCK" &&
+//             playerSelection.toUpperCase() !== "PAPER" &&
+//             playerSelection.toUpperCase() !== "SCISSOR") {
+//             alert("invalid input! Please Enter a valid input");
+//             i--;
+//             continue;
+//         }
+
+
+//         const computerSelection = computerPlay();
+//         console.log(playRound(playerSelection.toUpperCase(), computerSelection.toUpperCase()));
+
+//         if (playerScore === 5 || computerScore === 5) {
+//             break;
+//         }
+//     }
+
+//     if (playerScore === 5) {
+//         console.log("You won the game! Reload the page to play again");
+//     }
+//     else {
+//         console.log("You Lose the game! Reload the page to play again");
+//     }
+
+// }
